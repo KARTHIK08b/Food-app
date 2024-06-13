4ccff79f1616aec40e8d6412c6d4a794c21a79f9
KARTHIK08b <saikarthik0811@gmail.com>
2024-06-13 15:20:13 +0530

    Made changes in base.html, updated detail.html, index.html and item-form.html

--- food/urls.py
+++ food/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, items, detail, create_item, update_item,delete_item
+from .views import items, create_item, update_item,delete_item,IndexClassView,FoodDetail,CreateItem
 
 app_name = 'food'
 
 urlpatterns = [
     
-    path('', index, name='index'),
-    path('<int:item_id>/', detail, name='detail'),
+    path('', IndexClassView.as_view(), name='index'),
+    # path('<int:item_id>/', detail, name='detail'),
+    path('<int:pk>/', FoodDetail.as_view(), name='detail'),
+
     path('items', items, name='items'),
     #add items
-    path('add',create_item, name='create_item'),
+    path('add',CreateItem.as_view(), name='create_item'),
     #edit
     path('update/<int:id>/',update_item, name='update_item'),
     path('delete/<int:id>/',delete_item, name='delete_item'),
--- food/views.py
+++ food/views.py
@@ -1,31 +1,48 @@
+from django.forms import BaseModelForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 #content user is gonna view 
-def index(request):
-    item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
-    context = {
-        'item_list' : item_list,
-    }
-    # return HttpResponse(item_list)
-    # return HttpResponse(template.render(context,request))
-    return render(request,'food/index.html', context )
+# def index(request):
+#     item_list = Item.objects.all()
+#     template = loader.get_template('food/index.html')
+#     context = {
+#         'item_list' : item_list,
+#     }
+#     # return HttpResponse(item_list)
+#     # return HttpResponse(template.render(context,request))
+#     return render(request,'food/index.html', context )
+
+#the down code is a replacement for the above view. It is class based
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 
 def items(request):
     return HttpResponse('These are your items') 
 
-def detail(request, item_id):
-    item = Item.objects.get(pk = item_id)
-    context = {
-        'item': item, 
-    }
-    return render(request, 'food/detail.html', context)
-    # return HttpResponse("This is item no/id: %s" %item_id)
+
+# #function based view
+# def detail(request, item_id):
+#     item = Item.objects.get(pk = item_id)
+#     context = {
+#         'item': item, 
+#     }
+#     return render(request, 'food/detail.html', context)
+#     # return HttpResponse("This is item no/id: %s" %item_id)
+
+#class based view for detail
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
 
 def create_item(request):
     form = ItemForm(request.POST or None)
@@ -34,6 +51,19 @@ def create_item(request):
         form.save()
         return redirect('food:index')
     return render(request, 'food/item-form.html',{'form':form})
+
+#class based view for create item
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form) :
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
+   
+
+
 
 def update_item(request, id):
     item = Item.objects.get(id=id)
